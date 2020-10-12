49756e2623c11f85ebab6942ff11483da44b2c6b
nayan-bansal <nayan.7991@gmail.com>
2020-10-13 02:17:02 +0530

    Added: Binary Tree Main Program

---
+++ BinaryTree.java
@@ -0,0 +1,31 @@
+package Binary_Tree;
+
+public class BinaryTree<K extends Comparable<K>> {
+
+	private BinaryNode<K> root;
+
+	public void add(K key) {
+		this.root = this.addRecursively(root, key);
+	}
+
+	private BinaryNode<K> addRecursively(BinaryNode<K> current, K key) {
+		if (current == null)
+			return new BinaryNode<K>(key);
+		int compResult = key.compareTo(current.key);
+		if (compResult == 0)
+			return current;
+		if (compResult < 0) // if current>key -> left
+			current.left = addRecursively(current.left, key);
+		if (compResult > 0) // current<key ->right
+			current.right = addRecursively(current.right, key);
+		return current;
+	}
+
+	public int getSize() {
+		return this.getSizeRecursively(root);
+	}
+
+	private int getSizeRecursively(BinaryNode<K> current) {
+		return current == null ? 0 : 1 + this.getSizeRecursively(current.left) + this.getSizeRecursively(current.right);
+	}
+}
