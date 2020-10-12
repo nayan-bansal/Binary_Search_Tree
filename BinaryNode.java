2d49e0a0a8cf87c3d37cf497712ed02c31eb75cd
nayan-bansal <nayan.7991@gmail.com>
2020-10-13 02:19:27 +0530

    Added: Binary Tree Node

---
+++ BinaryNode.java
@@ -0,0 +1,17 @@
+package Binary_Tree;
+
+public class BinaryNode<K extends Comparable<K>> {
+
+	
+	K key;
+	BinaryNode<K> left;
+	BinaryNode<K> right;
+	
+	
+	public BinaryNode(K key) {
+		this.key = key;
+		this.left = null;
+		this.right = null;
+	}
+	
+}
