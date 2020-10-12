71a85e380e631b692f8c5330ae395bcd332edee0
nayan-bansal <nayan.7991@gmail.com>
2020-10-13 02:26:19 +0530

    Added: Full Binary Tree

--- BinaryTreeTest.java
+++ BinaryTreeTest.java
@@ -21,5 +21,25 @@ public class BinaryTreeTest {
 		Assert.assertEquals(3,size);
 		
 	}
-
+	@Test
+	public void createBinaryTreeCheckUsingSizeMethod() {
+	
+		BinaryTree<Integer> binaryTree = new BinaryTree<>();
+		binaryTree.add(56);
+		binaryTree.add(30);
+		binaryTree.add(70);
+		binaryTree.add(22);
+		binaryTree.add(40);
+		binaryTree.add(60);
+		binaryTree.add(95);
+		binaryTree.add(11);
+		binaryTree.add(65);
+		binaryTree.add(3);
+		binaryTree.add(16);
+		binaryTree.add(63);
+		binaryTree.add(67);
+		int size = binaryTree.getSize();
+		System.out.println("size is :" + size);
+		Assert.assertEquals(13, size);
+	}
 }
