49876de5db55262e197db35972121805c44bd2fa
nayan-bansal <nayan.7991@gmail.com>
2020-10-13 02:30:37 +0530

    Added: Searching Element in Binary Tree

--- BinaryTreeTest.java
+++ BinaryTreeTest.java
@@ -42,4 +42,24 @@ public class BinaryTreeTest {
 		System.out.println("size is :" + size);
 		Assert.assertEquals(13, size);
 	}
+
+	@Test
+	public void givenNumberWhenSearchedShouldBeFoundInBST() {
+		MyBinaryTree<Integer> binaryTree = new MyBinaryTree<>();
+		binaryTree.add(56);
+		binaryTree.add(30);
+		binaryTree.add(70);
+		binaryTree.add(22);
+		binaryTree.add(40);
+		binaryTree.add(60);
+		binaryTree.add(95);
+		binaryTree.add(11);
+		binaryTree.add(65);
+		binaryTree.add(3);
+		binaryTree.add(16);
+		binaryTree.add(63);
+		binaryTree.add(67);
+		int key = binaryTree.search(63);
+		Assert.assertEquals(63, key);
+	}
 }
